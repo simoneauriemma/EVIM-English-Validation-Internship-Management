773af17e8c5f0d0f3ae106abfe26fa6927fd690c
nicol <nicol@172.19.197.151>
2020-01-07 15:39:08 +0100

    finito questionario t

--- EVIM-English-Validation-Internship-Management/IM/model/QuestionariDAO.java
+++ EVIM-English-Validation-Internship-Management/IM/model/QuestionariDAO.java
@@ -42,5 +42,45 @@ public class QuestionariDAO {
 			
 		}
 	}
+	
+	public static boolean insertQuestionarioT(Questionario_t quest) {
+		String query= "INSERT INTO `evim`.`questionario_t` (`Email`, `ID_TutorAziendale`, `ComptenzeIngresso`, `CompetenzeAcquisite`, `Utilita`, `Motivazione`, `Capacita`, `Informazioni`, `Obiettivi`, `Servizi`, `Assistenza`, `Collaborazione`) VALUES ('?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?');";
+		try (Connection con = DriverManagerConnectionPool.getConnection()) {
+			PreparedStatement ps = con.prepareStatement(query);
+			
+			ps.setString(1, quest.getEmail());
+			ps.setInt(2, quest.getId_tutor());
+			ps.setInt(3, quest.getCompetenze_ingresso());
+			ps.setInt(4, quest.getCompetenze_acquisite());
+			ps.setInt(5, quest.getUtilita());
+			
+			ps.setInt(6, quest.getMotivazione());
+			ps.setInt(7, quest.getCapacita());
+			
+			ps.setInt(8, quest.getInformazioni());
+			ps.setInt(9, quest.getObiettivi());
+			ps.setInt(10,quest.getServizi());
+			ps.setInt(11, quest.getAssistenza());
+			ps.setInt(12, quest.getCollaborazione());
+
+		
+			
+			
+			
+			if(ps.executeUpdate()==1) {
+				return true;
+			}
+			else
+				return false;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+			throw new RuntimeException(e);
+			
+		}
+	}
+	
+	
 
 }
