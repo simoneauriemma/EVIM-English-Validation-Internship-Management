625520637bf9c7456da9668c59625127feb2f114
micheledur <micheleduraccio@hotmail.it>
2020-01-12 17:36:47 +0100

    cimmut prova

--- EVIM-English-Validation-Internship-Management/IM/test_GP/RimuoviPropostaTest.java
+++ EVIM-English-Validation-Internship-Management/IM/test_GP/RimuoviPropostaTest.java
@@ -24,7 +24,7 @@ public class RimuoviPropostaTest extends Mockito{
 	private Login servletLogin;
 	private MockHttpServletRequest request; 
 	private MockHttpServletResponse response;
-
+      
 	/**
 	 * Before.
 	 */
