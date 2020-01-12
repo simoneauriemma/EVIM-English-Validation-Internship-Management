fd205b4b7436a781857d2cb53dd2837f89b13e39
antoniogiano <antoniogiano@192.168.0.17>
2020-01-12 18:10:09 +0100

    Cancellata la classe Registro DAO Inserita la query nella classe attivitaDAO per settare la firma del responsabile sul DB. Creata la serlvet ApprovaAttivita

---
+++ EVIM-English-Validation-Internship-Management/IM/controller/GestioneRegistroTirocinio/ApprovaAttivita.java
@@ -0,0 +1,48 @@
+package controller.GestioneRegistroTirocinio;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.AttivitaDAO;
+/**
+ * @author Antonio Giano
+ * Questa servlet permette di gestire l'approvazione o il rifiuto di un registro cambiando definitivamente lo stato di tale registro sul database
+ */
+@WebServlet("/ApprovaAttivita")
+public class ApprovaAttivita extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sessione=request.getSession();
+		if(sessione.getAttribute("utenteLoggato")==null) {
+			request.getRequestDispatcher("./WEB-INF/login.jsp").forward(request, response);
+		}
+		else {
+			String tipoUtente=sessione.getAttribute("utenteLoggato").getClass().getName();
+			// solo il tutor aziendale e il tutor accademico possono approvare l'attivita
+			if(tipoUtente.equalsIgnoreCase("model.tutoraccademico") || tipoUtente.equalsIgnoreCase("model.tutoraziendale")) {
+				int idAttivita=Integer.parseInt(request.getParameter("idAttivita"));
+				String modifica=request.getParameter("modifica");
+				if(modifica.equalsIgnoreCase("approva")) 
+					if(!AttivitaDAO.changeFirmaResponsabile(idAttivita,1)) // 1= attivita approvata
+						throw new IllegalAccessError("Errore nell'approvare l'attivita");
+			}
+			//tutti gli altri utenti gli sono stati negati per l'approvazione di una attivita
+			else {
+				request.getRequestDispatcher("permissionDenied.jsp").forward(request, response);	
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
