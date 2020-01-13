274f807f74e06d3756009e76d32049d45659a64d
nicol <nicol@172.19.234.167>
2020-01-13 19:50:01 +0100

    servlet approva registro in completamento

--- EVIM-English-Validation-Internship-Management/IM/model/RegistroDAO.java
+++ EVIM-English-Validation-Internship-Management/IM/model/RegistroDAO.java
@@ -21,5 +21,80 @@ public class RegistroDAO {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public static boolean doAlterFirmaTutor(int idregistro) { // per tutor accademici che approvano il registro di tirocinio esterno
+		boolean result=false;
+		try (Connection con = DriverManagerConnectionPool.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement(
+					"UPDATE `evim`.`registro` SET `FirmaTutorAccamico` = '1' WHERE (`ID_Registro` = ? ) and (`FirmaResponsabile` = '1' );");
+			ps.setInt(1, idregistro);
+			
+			if(ps.executeUpdate()==1) {
+				result=true;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+	
+	public static boolean doAlterFirmaTutorInterno(int idregistro) { // per tutor accademici che approvano il registro di tirocinio interno
+		boolean result=false;
+		try (Connection con = DriverManagerConnectionPool.getConnection()) {
+			
+			PreparedStatement ps = con.prepareStatement( 
+					"UPDATE `evim`.`registro` SET `FirmaResponsabile` = '1', `FirmaTutorAccamico` = '1' WHERE (`ID_Registro` = ?); "); //il tutor firma
+			
+			ps.setInt(1, idregistro);
+			
+			
+			
+			if(ps.executeUpdate()==1) {
+				ps = con.prepareStatement("UPDATE `evim`.`registro` SET `Status` = 'approvato' WHERE (`ID_Registro` = ?);"); //il registro diventa approvato
+				ps.setInt(1, idregistro);
+				if(ps.executeUpdate()==1) {
+					result=true;
+				}
+			
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+	
+	public static boolean doAlterFirmaResponsabile(int idregistro,int idtirocinio) { // per pdcd che approva un registro di tirocinio esterno
+		boolean result=false;
+		try (Connection con = DriverManagerConnectionPool.getConnection()) {
+			String questionario;
+			String relazione;
+			PreparedStatement ps = con.prepareStatement(
+					"SELECT questionario_t.ID_Questionario, questionario_s.ID_Questionario, relazione.ID_Relazione from tirocinioesterno join "
+					+ "questionario_t on tirocinioesterno.EMAIL=questionario_t.Email join tirocinioesterno.email=questionario_s.Email "
+					+ "join relazione on relazione.Email=tirocinioesterno.EMAIL where tirocinioesterno.ID_TirocinioEsterno= ?");
+					
+			ps.setInt(1, idtirocinio);
+			
+			if(ps.executeUpdate()==1) {
+				result=true;
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+	
+	
 
 }
