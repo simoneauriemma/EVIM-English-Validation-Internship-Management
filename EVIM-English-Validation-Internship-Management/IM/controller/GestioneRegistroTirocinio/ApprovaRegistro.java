2ca32b4e5293a69beb609988f43c14110faa7b90
nicol <nicol@172.19.234.167>
2020-01-13 19:50:01 +0100

    servlet approva registro in completamento

---
+++ EVIM-English-Validation-Internship-Management/IM/controller/GestioneRegistroTirocinio/ApprovaRegistro.java
@@ -0,0 +1,90 @@
+package controller.GestioneRegistroTirocinio;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.GestioneAutenticazione.BaseServlet;
+import model.RegistroDAO;
+
+/**
+ * Servlet implementation class ApprovaRegistro
+ */
+@WebServlet("/ApprovaRegistro")
+public class ApprovaRegistro extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ApprovaRegistro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		String name = (String) session.getAttribute("type");
+		int idregistro = Integer.parseInt((String) request.getParameter("idregistro"));
+		
+		boolean result = false;
+		String tipotirocinio= (String) request.getParameter("tipotirocinio");
+		
+		//per tutor accademico
+		
+		if (name.equals("tutoraccademico")) {
+			
+			//caso di tirocinio interno --> firmo come responsabile e come tutor
+			if(tipotirocinio.equals("interno")) {
+				//tirocinio interno approva e cambio lo status del registro e del tirocinio
+				
+				result = RegistroDAO.doAlterFirmaTutorInterno(idregistro);
+			
+			
+			}
+			else { // caso di tirocinio esterno firmo solo come tutor accademico
+				result = RegistroDAO.doAlterFirmaTutor(idregistro);
+			}
+			
+			
+		}
+		
+		//pdcd
+		
+		else if (name.contentEquals("pdcd")) { // pdcd firma come responsabile per il tirocinio esterno
+			
+			int idtirocinio = Integer.parseInt((String) request.getParameter("idtirocinio"));
+			
+			result = RegistroDAO.doAlterFirmaResponsabile(idregistro,idtirocinio);
+
+		}
+		request.setAttribute("resultapprovaregistro", result);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/listaTirocini.jsp");
+		dispatcher.forward(request, response);
+		
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
